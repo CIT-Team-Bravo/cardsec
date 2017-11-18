54399bc8915d2320cc718ec6bd0f139f7e0c9126
ahernd2 <david.ahern@dell.com>
2017-11-18 18:40:50 +0000

    Added google-api, and test with powermock

--- src/main/java/ie/cit/teambravo/cardsec/CardsecApplication.java
+++ src/main/java/ie/cit/teambravo/cardsec/CardsecApplication.java
@@ -1,14 +1,29 @@
 package ie.cit.teambravo.cardsec;
 
+import com.google.maps.GeoApiContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication(scanBasePackageClasses = {
 		CardsecApplication.class
 })
 public class CardsecApplication {
 
+	@Value("${google.api.distance.key}")
+	private String API_KEY;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardsecApplication.class, args);
+	}
+
+	@Bean
+	public GeoApiContext geoApiContext(ApplicationContext ctx) {
+		return new GeoApiContext.Builder().apiKey(API_KEY).build();
 	}
 }
---
+++ src/test/java/ie/cit/teambravo/cardsec/services/DistanceServiceTests.java
@@ -0,0 +1,57 @@
+package ie.cit.teambravo.cardsec.services;
+
+import com.google.maps.*;
+import com.google.maps.model.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({GeocodingApi.class, DistanceMatrixApiRequest.class, DistanceMatrixApi.class})
+public class DistanceServiceTests {
+
+    @Mock
+    private GeoApiContext geoApiContext;
+
+    @InjectMocks
+    private DistanceService distanceService;
+
+    @Test
+    public void should_get_distance_between_2_points() throws Exception {
+        // Arrange
+        LatLng start = new LatLng(14, 22);
+        LatLng end = new LatLng(15, 10);
+        DistanceMatrixRow distanceMatrixRow = new DistanceMatrixRow();
+        DistanceMatrixRow[] distanceMatrixRows = new DistanceMatrixRow[]{distanceMatrixRow};
+        String[] startLoc = new String[]{"address start"};
+        String[] endLoc = new String[]{"address end"};
+
+        DistanceMatrix distanceMatrix = new DistanceMatrix(startLoc, endLoc, distanceMatrixRows);
+
+        DistanceMatrixApiRequest distanceMatrixApiRequest = mock(DistanceMatrixApiRequest.class);
+        mockStatic(DistanceMatrixApi.class);
+        when(distanceMatrixApiRequest.origins(start)).thenReturn(distanceMatrixApiRequest);
+        when(distanceMatrixApiRequest.destinations(end)).thenReturn(distanceMatrixApiRequest);
+        when(distanceMatrixApiRequest.mode(TravelMode.DRIVING)).thenReturn(distanceMatrixApiRequest);
+        when(distanceMatrixApiRequest.await()).thenReturn(distanceMatrix);
+
+        when(DistanceMatrixApi.newRequest(geoApiContext)).thenReturn(distanceMatrixApiRequest);
+
+        // Act
+        DistanceMatrix matrix = distanceService.getDistanceBetween2Points(start, end);
+
+        // Assert
+        assertNotNull(matrix);
+        assertThat(matrix, is(distanceMatrix));
+    }
+}
