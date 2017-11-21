05df6635e33db8b086050f38ef393dc198ccf196
Shane Treacy <shane.treacy@emc.com>
2017-11-21 22:59:36 +0000

    add test & impl for message structure

--- src/main/java/ie/cit/teambravo/cardsec/alerts/Alert.java
+++ src/main/java/ie/cit/teambravo/cardsec/alerts/Alert.java
@@ -3,24 +3,26 @@ package ie.cit.teambravo.cardsec.alerts;
 import ie.cit.teambravo.cardsec.dto.EventDto;
 
 public class Alert {
+	private final String severity = "High";
+	private final String title = "Possible Cloned Access Card";
 
 	private EventDto currentEvent;
 	private EventDto previousEvent;
+
+	public String getSeverity() {
+		return severity;
+	}
+
+	public String getTitle() {
+		return title;
+	}
 
 	public EventDto getCurrentEvent() {
 		return currentEvent;
 	}
 
-	public void setCurrentEvent(EventDto currentEvent) {
-		this.currentEvent = currentEvent;
-	}
-
 	public EventDto getPreviousEvent() {
 		return previousEvent;
-	}
-
-	public void setPreviousEvent(EventDto previousEvent) {
-		this.previousEvent = previousEvent;
 	}
 
 	public Alert(EventDto currentEvent, EventDto previousEvent) {
--- src/test/java/ie/cit/teambravo/cardsec/alerts/MessagingAlertDistributorTest.java
+++ src/test/java/ie/cit/teambravo/cardsec/alerts/MessagingAlertDistributorTest.java
@@ -1,5 +1,6 @@
 package ie.cit.teambravo.cardsec.alerts;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -8,22 +9,31 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+
 import ie.cit.teambravo.cardsec.dto.EventDto;
 
 public class MessagingAlertDistributorTest {
 
 	private MessageGateway mockEventGateway;
 	private MessagingAlertDistributor messagingAlertDistributor;
+	private JsonSchema schema;
+	private ObjectMapper mapper;
 
 	@Before
-	public void setup() {
+	public void setup() throws ProcessingException {
 		mockEventGateway = Mockito.mock(MessageGateway.class);
-
 		messagingAlertDistributor = new MessagingAlertDistributor(mockEventGateway);
+		schema = JsonSchemaFactory.byDefault().getJsonSchema("resource:/messageSchema.json");
+		mapper = new ObjectMapper();
 	}
 
 	@Test
-	public void generateAlert_when_called_then_theMessageIsFormattedCorrectly() {
+	public void generateAlert_when_called_then_theMessageIsFormattedCorrectly() throws Exception {
 		// Arrange
 		EventDto currentEvent = new EventDto();
 		EventDto previousEvent = new EventDto();
@@ -35,5 +45,9 @@ public class MessagingAlertDistributorTest {
 
 		// Assert
 		verify(mockEventGateway, times(1)).sendToMqtt(captor.capture());
+		String jsonPayload = captor.getValue().getPayload();
+
+		ProcessingReport result = schema.validate(mapper.readTree(jsonPayload), true);
+		assertTrue(result.isSuccess());
 	}
 }
