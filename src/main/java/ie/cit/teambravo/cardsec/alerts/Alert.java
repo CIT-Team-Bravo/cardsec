7d222cc2cf38e499326c5d07a5663d92bdeabfc9
Shane Treacy <shane.treacy@emc.com>
2017-11-22 10:27:10 +0000

    Add test & then description field

--- src/main/java/ie/cit/teambravo/cardsec/alerts/Alert.java
+++ src/main/java/ie/cit/teambravo/cardsec/alerts/Alert.java
@@ -5,9 +5,15 @@ import ie.cit.teambravo.cardsec.dto.EventDto;
 public class Alert {
 	private final String severity = "High";
 	private final String title = "Possible Cloned Access Card";
+	private final String description = "An access-card has been used that was very recently used in another location, indicating that it is unlikely to be the same card-holder";
 
 	private EventDto currentEvent;
 	private EventDto previousEvent;
+
+	public Alert(EventDto currentEvent, EventDto previousEvent) {
+		this.currentEvent = currentEvent;
+		this.previousEvent = previousEvent;
+	}
 
 	public String getSeverity() {
 		return severity;
@@ -25,9 +31,8 @@ public class Alert {
 		return previousEvent;
 	}
 
-	public Alert(EventDto currentEvent, EventDto previousEvent) {
-		this.currentEvent = currentEvent;
-		this.previousEvent = previousEvent;
+	public String getDescription() {
+		return description;
 	}
 
 }
