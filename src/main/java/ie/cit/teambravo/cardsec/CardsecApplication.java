2a2ea0ca4066a479bcc7f32e5744f3bee462c6e4
Stephen OBrien <stephen.obrien4@emc.com>
2017-11-22 18:03:44 +0000

    Remove unused imports

--- src/main/java/ie/cit/teambravo/cardsec/CardsecApplication.java
+++ src/main/java/ie/cit/teambravo/cardsec/CardsecApplication.java
@@ -2,19 +2,16 @@ package ie.cit.teambravo.cardsec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
 
 @EnableCaching
 @SpringBootApplication(scanBasePackageClasses = {
-        CardsecApplication.class
+		CardsecApplication.class
 })
 public class CardsecApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CardsecApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CardsecApplication.class, args);
+	}
 
 }
